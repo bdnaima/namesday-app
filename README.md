4c91cbfce8695c91b867d80cb9587619a9b9c1d0
Naima Malik <naimamaria.malik@medieinstitutet.se>
2020-01-11 23:48:52 +0100

    Added input field in html and little code in app.js.

---
+++ README.md
@@ -0,0 +1,5 @@
+Names day mini-web-app
+
+1. User requests name in input field and gets the date of names day.
+2. After name is requested the user can also see other names for that day.
+3. User can also request date and see all names for that day.
---
+++ assets/scripts/app.js
@@ -0,0 +1,13 @@
+
+
+const buttonEl = document.querySelector("#button");
+const userInputEl = document.querySelector("#userInput")
+
+buttonEl.addEventListener('click', function() {
+
+   const userInput = userInputEl.value;
+
+   console.log(userInput);
+
+
+});
