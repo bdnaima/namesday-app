5c026b6cef850cb16c9bde818bbf1707c1ea1de0
Naima Malik <naimamaria.malik@medieinstitutet.se>
2020-01-12 17:22:56 +0100

    Added new input field for country and button for testing. Coded for innerHTML and working on second fetch.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,17 +3,36 @@
 
 const buttonEl = document.querySelector("#button");
 const userInputEl = document.querySelector("#userInput")
-
+// const userInputCountry = document.querySelector("#userInputCountry")
+ 
 buttonEl.addEventListener('click', async function() {
 
     const userInput = userInputEl.value;
+   // const userInputTwo = userInputCountry.value;
+
+
     const response = await fetch(`https://api.abalin.net/getdate?name=${userInput}&country=se`);
     const data = await response.json()
     console.log(data);
 
-    
+    const date = await fetch(`https://api.abalin.net/namedays?&month=7&day=15`);
+    const result = await date.json();
+    console.log(result);
+
+    const nameInput = document.querySelector("#nameInput");
+    const dataResults = data.results;
+
+    let html = ""
+    dataResults.forEach((item) => {
+        html += `<p>${item.name}</p>
+                 <p>${item.month}</p>
+                 <p>${item.day}</p>`;
+    });
+
+    nameInput.innerHTML = html;
 
 
    console.log(userInput);
+//    console.log(userInputTwo);
 
 });
