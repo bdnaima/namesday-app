02399127e90bb81498c09254a42302ed323fc9e5
Naima Malik <naimamaria.malik@medieinstitutet.se>
2020-01-12 23:44:50 +0100

    Added select element for dropdown of countries and now able to get country, month and day.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,36 +3,43 @@
 
 const buttonEl = document.querySelector("#button");
 const userInputEl = document.querySelector("#userInput")
-// const userInputCountry = document.querySelector("#userInputCountry")
+const userInputCountry = document.querySelector("#userInputCountry")
  
 buttonEl.addEventListener('click', async function() {
 
     const userInput = userInputEl.value;
+    const country = userInputCountry.value
    // const userInputTwo = userInputCountry.value;
 
 
-    const response = await fetch(`https://api.abalin.net/getdate?name=${userInput}&country=se`);
+    const response = await fetch(`https://api.abalin.net/getdate?name=${userInput}&country=${country}`);
     const data = await response.json()
     console.log(data);
 
-    const date = await fetch(`https://api.abalin.net/namedays?&month=7&day=15`);
-    const result = await date.json();
-    console.log(result);
 
     const nameInput = document.querySelector("#nameInput");
-    const dataResults = data.results;
 
     let html = ""
-    dataResults.forEach((item) => {
+    data.results.forEach(async (item) => {
+        
+        const response = await fetch(`https://api.abalin.net/namedays?&month=${item.month}&day=${item.day}`);
+        const result = await response.json();
+
         html += `<p>${item.name}</p>
                  <p>${item.month}</p>
-                 <p>${item.day}</p>`;
+                 <p>${item.day}</p>
+                 <p>${result.data[0].namedays.fr}</p>`;
+
+        nameInput.innerHTML = html;
+
+        console.log(result);
+    
     });
 
-    nameInput.innerHTML = html;
+   
 
 
-   console.log(userInput);
+   //console.log(userInput);
 //    console.log(userInputTwo);
 
 });
