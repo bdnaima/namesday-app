959494a5d754676d86695c95e86f5dace490650f
Naima Malik <naimamaria.malik@medieinstitutet.se>
2020-01-12 14:26:36 +0100

    Added h2 and div element in  HTML. Used fetch to get data from names API.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,19 @@
 
 
+
 const buttonEl = document.querySelector("#button");
 const userInputEl = document.querySelector("#userInput")
 
-buttonEl.addEventListener('click', function() {
+buttonEl.addEventListener('click', async function() {
 
-   const userInput = userInputEl.value;
+    const userInput = userInputEl.value;
+    const response = await fetch(`https://api.abalin.net/getdate?name=${userInput}&country=se`);
+    const data = await response.json()
+    console.log(data);
+
+    
+
 
    console.log(userInput);
-
 
 });
