57686fdfa0d45b2d1fcfce05233ea2a84b0ce084
Naima Malik <naimamaria.malik@medieinstitutet.se>
2020-01-14 12:14:08 +0100

    More changes.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Names day mini-web-app
 1. User requests name in input field and gets the date of names day.
 2. After name is requested the user can also see other names for that day.
 3. User can also request date and see all names for that day.
+4. User can select country and enter a name and see which names appear for that particular country.
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -19,16 +19,19 @@ buttonEl.addEventListener('click', async function() {
 
     const nameInput = document.querySelector("#nameInput");
 
-    let html = ""
+    let html = "";
     data.results.forEach(async (item) => {
         
         const response = await fetch(`https://api.abalin.net/namedays?&month=${item.month}&day=${item.day}`);
         const result = await response.json();
 
-        html += `<p>${item.name}</p>
-                 <p>${item.month}</p>
-                 <p>${item.day}</p>
-                 <p>${result.data[0].namedays.fr}</p>`;
+        html += `<h1>${item.name}</h1>
+                <h2>Month</h2>
+                <h3>${item.month}</h3>
+                <h2>Day</h2>
+                <h3>${item.day}</h3>
+                <h2>Other names around the world</h2>
+                <p>${result.data[0].namedays}</p>`;
 
         nameInput.innerHTML = html;
 
