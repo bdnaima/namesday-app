cc1aa666122052649768373d6b841bdd51e95617
Naima Malik <naimamaria.malik@medieinstitutet.se>
2020-01-16 08:44:27 +0100

    Separating js files and playing around with layout and style.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,7 @@
 
 
+
+// Getting name
 const buttonEl = document.querySelector("#button");
 const userInputEl = document.querySelector("#userInput");
 const userInputCountry = document.querySelector("#userInputCountry");
@@ -9,47 +11,41 @@ buttonEl.addEventListener('click', async function() {
     const userInput = userInputEl.value;
     const country = userInputCountry.value;
 
-
-    const response = await fetch(`https://api.abalin.net/getdate?name=${userInput}&country=${country}`)
-    const data = await response.json()
-    console.log(data)
-
+    const data = await fetchDate(userInput, country);
 
     const nameInput = document.querySelector("#nameInput");
-
     let html = "";
 
-
-    data.results.forEach(async (item) => {
-        
-  
-        html += `<h2>${item.name}</h2>
-                <h3 style = "background-color: red; width: 50%; text-align: center;">${item.month}</h3>
-                <h3 style="background-color: white; color: black; width: 50%; text-align: center;">${item.day}</h3>`;
-
+    
+    data.results.forEach((item) => {
+          
+        html += `<h2 style = "text-align: center; color: black;">${item.name}</h2>
+                <h3 style = "background-color: brown; width: 50%; text-align: center;">${item.month}</h3>
+                <h3 style="background-color: white; color: black; width: 50%; text-align: center;">${item.day}</h3>
+                `;
         nameInput.innerHTML = html;
-
-        console.log(result);
     
     });
 
 });
 
-// Getting the dat  e
+// Getting the date
 const userInputMonth = document.querySelector("#userInputMonth");
+const userInputDay = document.querySelector("#userInputDay")
 const dateButton = document.querySelector("#dateButton")
 
 dateButton.addEventListener('click', async function() {
-    const dateValue = userInputMonth.value;
     const country = userInputCountry.value;
+    const dateValue = userInputMonth.value;
+    const dayValue = userInputDay.value
+    
 
-    const responseDate = await fetch(`https://api.abalin.net/namedays?country=${country}&month=${dateValue}&day=15`);
-    const result = await responseDate.json();
+    const result = await fetchName(country, dateValue, dayValue)
 
     console.log(result);
 
     const nameInput = document.querySelector('#nameInput');
     const resultNames = result.data[0].namedays[country];
 
-    nameInput.innerHTML = `<p>${resultNames}</p>`;
+    nameInput.innerHTML = `<h2>${resultNames}<h2>`;
 });
--- assets/scripts/namesdayabalin.js
+++ assets/scripts/namesdayabalin.js
@@ -2,3 +2,17 @@
  * Names day Abalin
  */
 
+ async function fetchDate(name, country) {
+
+    const response = await fetch(`https://api.abalin.net/getdate?name=${name}&country=${country}`)
+    const data = await response.json()
+    console.log(data)
+    return data;
+ }
+
+ async function fetchName(country, month, day) {
+    const responseDate = await fetch(`https://api.abalin.net/namedays?country=${country}&month=${month}&day=${day}`);
+    const result = await responseDate.json();
+    return result;
+ }
+
